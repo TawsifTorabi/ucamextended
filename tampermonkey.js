e48c6ee95822700b9d8eb14a6facc1781327c05c
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-07-28 18:44:37 +0600

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -2,22 +2,18 @@
 // @name         UCAM Extended
 // @namespace    http://tampermonkey.net/
 // @version      0.1.1
-// @description  Having Pera lying to your dad? Here it is!
+// @description  Adds Functionality to UIU UCAM Website
 // @author       Tawsif Torabi
 // @match        *://ucam.uiu.ac.bd/*
 // @icon         https://www.google.com/s2/favicons?domain=ac.bd
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @require      https://cdn.jsdelivr.net/npm/table-to-json@1.0.0/lib/jquery.tabletojson.min.js
-// @downloadURL  https://raw.githubusercontent.com/TawsifTorabi/UCAMextended/main/tampermonkey.js
-// @updateURL    https://raw.githubusercontent.com/TawsifTorabi/UCAMextended/main/tampermonkey.js
 // @grant        unsafeWindow
 // ==/UserScript==
 
 (function() {
     'use strict';
-	
-    var sampler;
 
     //Declare Datas
     var Aurna = [
@@ -404,7 +400,8 @@
                     uiucsecontents+
                     'Peace, Over and Out 🕶</br>'+
                     '</p></br>'+
-                    '<span style=\'font-size: 15px;\'><input id=\'HideFresherMsg\' type=\'checkbox\'> Don\'t show this Message Again</br></span>';
+                    '<span style=\'font-size: 15px;\'><input id=\'HideFresherMsg\' type=\'checkbox\'> Don\'t show this Message Again</br></span>'+
+                    '<a target=\'_blank\'href=\'https://tawsiftorabi.github.io/ucamextended/#bug\'>REPORT A BUG</a>';
 
 
                 aurnaIframe(tt);
@@ -813,7 +810,7 @@
         + "</a>&nbsp;&nbsp;"
         + "<a class='about-button' href='"+metaData[1]+"' target='_blank' style='padding: 5px 10px; text-decoration:none;'>"
         + "<svg id='miniIcons' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z'/></svg>"
-        + "Code On Github</a></br></br></center>"
+        + "Code On Github</a></br></br><a target=\'_blank\'href=\'https://tawsiftorabi.github.io/ucamextended/#bug\'>REPORT A BUG</a></center>"
         ;
 
         var settingBtn = document.createElement("button");
