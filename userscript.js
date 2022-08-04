a169a1b1d16b19edc198ea0fffb83718d42df51f
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-08-05 02:39:06 +0600

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,14 +1,11 @@
 // ==UserScript==
 // @name         UCAM Extended
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
-// @description  A Web Plugin for UIU UCAM Portal to add more functionalities.
+// @version      0.1.3
+// @description  Having Pera lying to your dad? Here it is!
 // @author       Tawsif Torabi
 // @match        *://ucam.uiu.ac.bd/*
 // @icon         https://www.google.com/s2/favicons?domain=ac.bd
-// @grant        none
-// @require      https://code.jquery.com/jquery-3.5.1.min.js
-// @require      https://cdn.jsdelivr.net/npm/table-to-json@1.0.0/lib/jquery.tabletojson.min.js
 // @grant        unsafeWindow
 // ==/UserScript==
 
@@ -25,7 +22,7 @@
         'https://facebook.com/tawsiftorabi',
         'https://github.com/TawsifTorabi/UCAMextended',
         'UCAM Extended Plugin',
-        'v0.1.1 Beta',
+        'v0.1.3 Beta',
         'Tawsif Torabi',
         'http://tawsiftorabi.github.io'
     ];
